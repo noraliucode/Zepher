5cc42cc049aaa0333e4e1b03c53ea655e3e3898f
Nora Liu <noraliu0830@gmail.com>
2023-05-20 02:39:13 +0800

    clean up

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -12,9 +12,6 @@ import {
   ListItem,
   Divider,
 } from "@ui-kitten/components";
-import { EvaIconsPack } from "@ui-kitten/eva-icons";
-import * as eva from "@eva-design/eva";
-import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 
 const Dashboard = () => {
   const navigation = useNavigation();
@@ -54,24 +51,21 @@ const Dashboard = () => {
 
   return (
     <>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <Layout style={{ flex: 1, padding: 16 }}>
-          <Text category="h1" style={{ marginBottom: 16 }}>
-            Dashboard
-          </Text>
-          <List
-            data={goals}
-            renderItem={renderItem}
-            ItemSeparatorComponent={Divider}
-            style={{ marginBottom: 16 }}
-            numColumns={2}
-          />
-          <Button onPress={() => navigation.navigate("GoalSetup")}>
-            Set New Goal
-          </Button>
-        </Layout>
-      </ApplicationProvider>
+      <Layout style={{ flex: 1, padding: 16 }}>
+        <Text category="h1" style={{ marginBottom: 16 }}>
+          Dashboard
+        </Text>
+        <List
+          data={goals}
+          renderItem={renderItem}
+          ItemSeparatorComponent={Divider}
+          style={{ marginBottom: 16 }}
+          numColumns={2}
+        />
+        <Button onPress={() => navigation.navigate("GoalSetup")}>
+          Set New Goal
+        </Button>
+      </Layout>
     </>
   );
 };
